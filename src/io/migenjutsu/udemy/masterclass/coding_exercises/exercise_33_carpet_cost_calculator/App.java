bd840f4023d35e1b8c2317ba6cb138d97c9fee95
domHenin <domhenin.517@gmail.com>
2019-09-26 12:27:09 -0400

    challenge complete

---
+++ src/io/migenjutsu/udemy/masterclass/coding_exercises/exercise_33_carpet_cost_calculator/App.java
@@ -0,0 +1,19 @@
+package io.migenjutsu.udemy.masterclass.coding_exercises.exercise_33_carpet_cost_calculator;
+
+public class App {
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+    }
+}
+
+//→ OUTPUT
+//
+//total= 38.5
+//total= 36.45
---
+++ src/io/migenjutsu/udemy/masterclass/coding_exercises/exercise_33_carpet_cost_calculator/Calculator.java
@@ -0,0 +1,15 @@
+package io.migenjutsu.udemy.masterclass.coding_exercises.exercise_33_carpet_cost_calculator;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
